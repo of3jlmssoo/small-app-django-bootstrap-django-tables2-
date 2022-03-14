fc990e0a3cb9a184101ac15f3037bbf6bdf7d824
of3jlmssoo <7a4wiy5vd638@gmail.com>
2022-03-14 20:05:56 +0900

    min fields in html

--- tabletestproject/settings.py
+++ tabletestproject/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ["192.168.10.109"]
+ALLOWED_HOSTS = ["192.168.10.109", "127.0.0.1"]
 
 
 # Application definition
