4bfd1f77af207704c0f08cf03248d34e5da44959
of3jlmssoo <7a4wiy5vd638@gmail.com>
2022-03-22 13:46:25 +0900

    import export for admin

--- tabletest/admin.py
+++ tabletest/admin.py
@@ -5,10 +5,20 @@ from django.contrib import admin
 
 from .models import ProductOrder
 
-class ProductOrderAdmin(admin.ModelAdmin):
-    list_display = ( 'id', 'goods', 'product_price', 'type_of_estimation', 'product_type', 'product_use', 'alternative', 'expected_purchase_date', 'created_on', 'updated_on',)
+from import_export import resources
+from import_export.admin import  ImportExportModelAdmin
 
+class ProductOrderResource(resources.ModelResource):
+    class Meta:
+        model = ProductOrder
+
+# class ProductOrderAdmin(admin.ModelAdmin):
+class ProductOrderAdmin(ImportExportModelAdmin):
+    list_display = ( 'id', 'goods', 'product_price', 'type_of_estimation', 'product_type', 'product_use', 'alternative', 'expected_purchase_date', 'created_on', 'updated_on',)
+    resource_class=ProductOrderResource
 
 
 
 admin.site.register(ProductOrder, ProductOrderAdmin)
+
+
--- tabletestproject/settings.py
+++ tabletestproject/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tabletest',
     'accounts',
+    'import_export',
 ]
 
 MIDDLEWARE = [
