579dfeddc18d48f7856fbdc535d020fef24f8c50
of3jlmssoo <7a4wiy5vd638@gmail.com>
2022-04-02 17:30:39 +0900

    default template changed to bootstrap4 tables

--- tabletest/urls.py
+++ tabletest/urls.py
@@ -6,7 +6,8 @@ from . import views
 
 urlpatterns = [
     path('place_order/', views.place_order, name='place_order'),
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('', views.bootstrap4, name='bootstrap4'),
     # path('', views.indexcopy, name='indexcopy'),
     path('confirm_details/', views.confirm_details, name='confirm_details'),
     path("bootstrap4/", views.bootstrap4, name="bootstrap4"),
