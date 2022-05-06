af9f3cb6b299aeda6ad521b0692ca206e2fdec96
of3jlmssoo <7a4wiy5vd638@gmail.com>
2022-05-06 13:46:04 +0900

    cleaning

--- tabletest/tables.py
+++ tabletest/tables.py
@@ -2,23 +2,6 @@ import django_tables2 as tables
 
 from .models import ProductOrder
 
-# class BootstrapTable(tables.Table):
-#     class Meta:
-#         model = ProductOrder
-#         template_name = "tabletest/bootstrap.html"
-#         fields = ("goods", "product_price", "type_of_estimation", "product_type", "product_use","alternative", "expected_purchase_date")
-#         # linkify = ("goods")
-
-
-# class ProductOrderTable(tables.Table):
-#     name = tables.Column()
-#     population = tables.Column()
-#     tz = tables.Column(verbose_name="time zone")
-#     visits = tables.Column()
-#     summary = tables.Column(order_by=("name", "population"))
-
-#     class Meta:
-#         model = ProductOrder
 
 class NumberColumn(tables.Column):
     def render(self, value):
@@ -35,37 +18,20 @@ class TruncatedTextColumn(tables.Column):
 class Bootstrap4Table(tables.Table):
 
     id = tables.Column(linkify=True)
-    # goods = tables.Column(attrs={
-    #     # "th": {"align": "justify"},
-    #     # "td": {"align": "center"},
-    #     # "th": {"align": "center"},
-    # })
     goods = TruncatedTextColumn(accessor='goods')
     type_of_estimation = TruncatedTextColumn(accessor='type_of_estimation')
-    # goods = tables.TemplateColumn('<data-toggle="tooltip" title="{{record.goods}}">{{record.goods|truncatewords:5}}')
 
     alternative = tables.columns.BooleanColumn(yesno='有,無')
     updated_on = tables.columns.DateColumn(short=True)
 
-    # product_price = tables.Column()
     product_price = NumberColumn(
         attrs={
             "td": {"align": "right"}
         }
     )
 
-    # country = tables.Column(linkify=True)
-    # continent = tables.Column(accessor="country__continent", linkify=True)
-
     class Meta:
         model = ProductOrder    # .objects.filter(product_type='D')
         template_name = "tabletest/bootstrap4.html"
         attrs = {"class": "table table-hover table-sm"}
         exclude = ("created_on", "status", "comment",)
-        # exclude = ("friendly",)
-
-    # def render_number(self, value):
-    #     return '{:0.0f}'.format(value)
-
-    # def __init__(self, *args, **kwargs):
-    #     self.columns['goods'].column.attrs = {"td": {"style": "width:1%;"}}
