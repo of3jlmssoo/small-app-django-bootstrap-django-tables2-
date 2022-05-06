99593f3860d0fc9a0b8326dd7e70df16133e0fdb
of3jlmssoo <7a4wiy5vd638@gmail.com>
2022-05-06 13:46:40 +0900

    cleaning

--- tabletest/models.py
+++ tabletest/models.py
@@ -4,18 +4,6 @@ from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
-# class ProductOrder(models.Model):
-
-#     class ProductType(models.TextChoices):
-#         DAILY = 'D', _('生活必需品')
-#         LUXURY = 'L', _('贅沢品')
-
-#     product_type = models.CharField(
-#         max_length=1,
-#         choices=ProductType.choices,
-#         default=ProductType.DAILY
-#     )
-
 
 class ProductOrder(models.Model):
 
@@ -45,7 +33,6 @@ class ProductOrder(models.Model):
     alternative = models.BooleanField(verbose_name="代替")
     expected_purchase_date = models.DateField(verbose_name="購入予定日")
 
-    # order_number =
     created_on = models.DateTimeField(auto_now_add=True)  # , verbose_name="作成日")
     updated_on = models.DateTimeField(auto_now=True, verbose_name="更新日")
 
@@ -69,9 +56,7 @@ class ProductOrder(models.Model):
 
 class Document(models.Model):
     title = models.CharField(max_length=200)
-    # file_field = models.FileField(upload_to="static/")
     file_field = models.FileField(upload_to="media/documents/")
-    # file_field = models.ImageField(upload_to="media/documents/")
     dateTimeOfUpload = models.DateTimeField(auto_now=True)
     order = models.ForeignKey(ProductOrder, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, verbose_name="申請者")
